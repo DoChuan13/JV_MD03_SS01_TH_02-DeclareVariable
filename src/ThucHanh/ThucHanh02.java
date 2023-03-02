055232e92e6d3f742b0c6950bed15cf5ad6a6659
DoChuan13 <chuando.9213@gmail.com>
2023-03-02 19:30:22 +0700

    Declare Variable

---
+++ src/ThucHanh/ThucHanh02.java
@@ -0,0 +1,16 @@
+package ThucHanh;
+
+public class ThucHanh02 {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5F;
+        boolean b = true;
+        char c = 'a';
+        String s = "Nam Định";
+        System.out.println("i: " + i);
+        System.out.println("f: " + f);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("a: " + s);
+    }
+}
